36b32ef50ffba9e3d45dae3f1ee82924b3506f1f
Kaiptor3 <68454740+Kaiptor3@users.noreply.github.com>
2024-11-30 22:20:50 -0300

    Saiba Mais alto contraste pronto

---
+++ Root/catálogo/carros/whitemode.js
@@ -0,0 +1,9 @@
+document.getElementById("contrast-toggle").addEventListener("click", function () {
+    const body = document.body;
+    const button = this;
+
+    // Alterna a classe de alto contraste
+    body.classList.toggle("high-contrast");
+
+
+});
