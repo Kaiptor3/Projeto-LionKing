726a2823708ef068c7f270d3d19eb6add09ee8a8
Fernando Júnio de Assis dos Santos <fjuniodeassis@gmail.com>
2025-04-14 10:11:42 -0300

    Update index.php

--- Root/index.php
+++ Root/index.php
@@ -2,7 +2,7 @@
 $page = $_GET['page'] ?? 'home';
 
 if ($page === 'home') {
-    include 'home.php'; // Agora inclui o HTML da home
+    include 'home.html'; // Agora inclui o HTML da home
 } else {
     include '404.php';  // Página de erro
     exit;
