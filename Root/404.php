04cfe5aefa4c4cc74bd886ccc40cc66cb87c0135
Fernando Júnio de Assis dos Santos <fjuniodeassis@gmail.com>
2025-05-20 10:34:26 -0300

    Atualizar o 404.php

--- Root/404.php
+++ Root/404.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404</title>
     <link rel="icon" type="image/png" href="../imgs/favicon.png" sizes="16x16">
-    <link rel="stylesheet" href="erro.css">
+    <link rel="stylesheet" href="/Root/erro.css">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Montserrat:wght@400&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
