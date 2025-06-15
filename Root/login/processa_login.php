ff22e6a72ad824ab132f73afe244da292c4254bb
Kaiptor3 <68454740+Kaiptor3@users.noreply.github.com>
2025-06-15 20:23:27 -0300

    Add files via upload

---
+++ Root/login/processa_login.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+require_once 'conexao.php';
+require_once 'log_helper.php'; // <- adicione essa linha
+
+unset($_SESSION['usuario_temp'], $_SESSION['resposta_correta'], $_SESSION['pergunta_2fa'], $_SESSION['tentativas_2fa']);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $stmt = $conn->prepare("SELECT idUsuario, nomeCompleto, email, idPermissao, senha, dataNascimento FROM usuario WHERE email = ?");
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
+
+    if ($resultado->num_rows === 1) {
+        $usuario = $resultado->fetch_assoc();
+
+
+        if (password_verify($senha, $usuario['senha'])) {
+            $_SESSION['usuario_temp'] = [
+                'idUsuario'     => $usuario['idUsuario'],
+                'nomeCompleto'  => $usuario['nomeCompleto'],
+                'email'         => $usuario['email'],
+                'idPermissao'   => $usuario['idPermissao']
+            ];
+
+            
+
+            $_SESSION['resposta_correta'] = $usuario['dataNascimento'];
+
+            header("Location: 2fa.php");
+            exit();
+        }
+    }
+
+    header("Location: login.php?erro=1");
+    exit();
+}
+?>
