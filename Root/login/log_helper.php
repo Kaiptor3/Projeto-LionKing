7123f3667e370752f65a746ce1bc518c52fe6c94
Kaiptor3 <68454740+Kaiptor3@users.noreply.github.com>
2025-06-15 20:22:56 -0300

    Delete Root/login directory

--- Root/login/log_helper.php
+++
@@ -1,7 +0,0 @@
-<?php
-function registrarLog($conn, $idUsuario, $acao) {
-    $stmt = $conn->prepare("INSERT INTO log_usuario (idUsuario, acao) VALUES (?, ?)");
-    $stmt->bind_param("is", $idUsuario, $acao);
-    $stmt->execute();
-}
-?>
