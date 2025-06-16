2d6ffff26cefce8cc40bc4a538467d843a095905
Kaiptor3 <68454740+Kaiptor3@users.noreply.github.com>
2025-06-15 23:47:41 -0300

    Update login.php

--- Root/login/login.php
+++ Root/login/login.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (isset($_SESSION['usuario'])) {
+    header('Location: /Projeto-LionKing-main/Root/index.php');
+    exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 
